96af93c3e8fb9c24fde2a487488d1ee770060d53
Maraxen <maraxencz1097@gmail.com>
2024-04-16 18:14:18 +0200

    improve logic for writing to microSD

--- Transmitter_HC-12/Transmitter_HC-12.ino
+++ Transmitter_HC-12/Transmitter_HC-12.ino
@@ -26,7 +26,8 @@ String serialZprava = "";
 String HC12Zprava = "";
 boolean serialKonecZpravy = false;
 boolean HC12KonecZpravy = false;
-boolean communicationEnabled = true; // Flag to control communication
+boolean communicationEnabled = true;
+boolean sdEnabled = true; // Flag to control communication
 unsigned long lastTransmissionTime = 0;
 
 Adafruit_BME280 bme;
@@ -45,16 +46,21 @@ void setup()
   }
   if (!SD.begin(SD_CS_PIN)) {
         Serial.println("SD card initialization failed");
+        sdEnabled = false;
         return;
     }
-  // Open or create a file for writing
   dataFile = SD.open("/data.csv", FILE_WRITE);
   if (!dataFile) {
-      Serial.println("Error opening data.csv");
+    Serial.println("Error opening data.csv");
+  } 
+  else {
+    // Check if file is empty
+    if (dataFile.size() == 0) {
+      // Write header to the file
+      dataFile.println("Temperature;Humidity;Pressure;Latitude;Longitude;Time;Speed;Altitude;");
+    }
+    dataFile.close(); // Close the file after writing header
   }
-  // Write header to the file
-  dataFile.println("Temperature;Humidity;Pressure;Latitude;Longitude;Time;Speed;Altitude;");
-
   pinMode(pinSet, OUTPUT);
   digitalWrite(pinSet, HIGH);
   delay(80);
@@ -98,6 +104,19 @@ void loop()
       communicationEnabled = false;
       Serial.println("Communication disabled.");
       HC12.println("Communication disabled.");
+    }
+    else if (serialZprava.startsWith("COM+SD+ON"))
+    {
+      sdEnabled = true;
+      Serial.println("SD enabled.");
+      HC12.println("COM+OFF");
+      
+    }
+    else if (serialZprava.startsWith("COM+SD+OFF"))
+    {
+      sdEnabled = false;
+      Serial.println("SD disabled.");
+      HC12.println("COM+OFF");
     }
     else if (serialZprava.startsWith("AT")) 
     {
@@ -158,7 +177,7 @@ void loop()
   }
 
   
-  if (communicationEnabled && currentTime - lastTransmissionTime >= 500)
+  if (communicationEnabled && currentTime - lastTransmissionTime >= 100)
     {
       if (ss.available() > 0)
       {
@@ -166,75 +185,16 @@ void loop()
         {
           if (gps.location.isValid())
           {
-            HC12.print(bme.readTemperature());
-            HC12.print(";");
-            HC12.print(bme.readHumidity());
-            HC12.print(";");
-            HC12.print(bme.readPressure() / 100.0F);
-            HC12.print(";");
-            HC12.print(gps.location.lat(), 6);
-            HC12.print(F(";"));
-            HC12.print(gps.location.lng(), 6);
-            HC12.print(F(";"));
-            HC12.print(gps.time.hour());
-            HC12.print(F(":"));
-            HC12.print(gps.time.minute());
-            HC12.print(F(":"));
-            HC12.print(gps.time.second());
-            HC12.print(F("."));
-            HC12.print(gps.time.centisecond());
-            HC12.print(";");
-            HC12.print(gps.speed.kmph());
-            HC12.print(";");
-            HC12.print(gps.altitude.meters());
-            HC12.println();
-            
-            dataFile.print(bme.readTemperature());
-            dataFile.print(";");
-            dataFile.print(bme.readHumidity());
-            dataFile.print(";");
-            dataFile.print(bme.readPressure() / 100.0F);
-            dataFile.print(";");
-            dataFile.print(gps.location.lat(), 6);
-            dataFile.print(";");
-            dataFile.print(gps.location.lng(), 6);
-            dataFile.print(";");
-            dataFile.print(gps.time.hour());
-            dataFile.print(":");
-            dataFile.print(gps.time.minute());
-            dataFile.print(":");
-            dataFile.print(gps.time.second());
-            dataFile.print(".");
-            dataFile.print(gps.time.centisecond());
-            dataFile.print(";");
-            dataFile.print(gps.speed.kmph());
-            dataFile.print(";");
-            dataFile.print(gps.altitude.meters());
-            dataFile.println();
-            dataFile.flush();
+            // Transmit data via HC12
+            transmitDataHC12();
 
-            Serial.print(bme.readTemperature());
-            Serial.print(";");
-            Serial.print(bme.readHumidity());
-            Serial.print(";");
-            Serial.print(bme.readPressure() / 100.0F);
-            Serial.print(";");
-            Serial.print(gps.location.lat(), 6);
-            Serial.print(F(";"));
-            Serial.print(gps.location.lng(), 6);
-            Serial.print(F(";"));
-            Serial.print(gps.time.hour());
-            Serial.print(F(":"));
-            Serial.print(gps.time.minute());
-            Serial.print(F(":"));
-            Serial.print(gps.time.second());
-            Serial.print(F("."));
-            Serial.print(gps.time.centisecond());
-            Serial.print(";");
-            Serial.print(gps.speed.kmph());
-            Serial.print(";");
-            Serial.print(gps.altitude.meters());
-            Serial.println();
+            // Write data to SD card
+            if (sdEnabled) {
+              writeDataToFile();
+            }
+            // Print data to Serial
+            printDataToSerial();
+
             lastTransmissionTime = currentTime;
           }
           else
@@ -245,4 +205,89 @@ void loop()
         }
       }
     }
+}
+
+void transmitDataHC12() {
+  // Code to transmit data via HC12
+      HC12.print(bme.readTemperature());
+      HC12.print(bme.readHumidity());
+      HC12.print(";");
+      HC12.print(";");
+      HC12.print(bme.readPressure() / 100.0F);
+      HC12.print(";");
+      HC12.print(gps.location.lat(), 6);
+      HC12.print(F(";"));
+      HC12.print(gps.location.lng(), 6);
+      HC12.print(F(";"));
+      HC12.print(gps.time.hour());
+      HC12.print(F(":"));
+      HC12.print(gps.time.minute());
+      HC12.print(F(":"));
+      HC12.print(gps.time.second());
+      HC12.print(F("."));
+      HC12.print(gps.time.centisecond());
+      HC12.print(";");
+      HC12.print(gps.speed.kmph());
+      HC12.print(";");
+      HC12.print(gps.altitude.meters());
+      HC12.println();
+}
+
+void writeDataToFile() {
+  // Write data to the file
+  dataFile = SD.open("/data.csv", FILE_APPEND);
+  if (dataFile) {
+    dataFile.print(bme.readTemperature());
+    dataFile.print(";");
+    dataFile.print(bme.readHumidity());
+    dataFile.print(";");
+    dataFile.print(bme.readPressure() / 100.0F);
+    dataFile.print(";");
+    dataFile.print(gps.location.lat(), 6);
+    dataFile.print(F(";"));
+    dataFile.print(gps.location.lng(), 6);
+    dataFile.print(F(";"));
+    dataFile.print(gps.time.hour());
+    dataFile.print(F(":"));
+    dataFile.print(gps.time.minute());
+    dataFile.print(F(":"));
+    dataFile.print(gps.time.second());
+    dataFile.print(F("."));
+    dataFile.print(gps.time.centisecond());
+    dataFile.print(";");
+    dataFile.print(gps.speed.kmph());
+    dataFile.print(";");
+    dataFile.print(gps.altitude.meters());
+    dataFile.println();
+    dataFile.close();
+  }
+  else {
+    dataFile.close();
+  }
+}
+
+void printDataToSerial() {
+  // Print data to Serial
+  Serial.print(bme.readTemperature());
+  Serial.print(";");
+  Serial.print(bme.readHumidity());
+  Serial.print(";");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.print(";");
+  Serial.print(gps.location.lat(), 6);
+  Serial.print(F(";"));
+  Serial.print(gps.location.lng(), 6);
+  Serial.print(F(";"));
+  Serial.print(gps.time.hour());
+  Serial.print(F(":"));
+  Serial.print(gps.time.minute());
+  Serial.print(F(":"));
+  Serial.print(gps.time.second());
+  Serial.print(F("."));
+  Serial.print(gps.time.centisecond());
+  Serial.print(";");
+  Serial.print(gps.speed.kmph());
+  Serial.print(";");
+  Serial.print(gps.altitude.meters());
+  Serial.println();
 }
